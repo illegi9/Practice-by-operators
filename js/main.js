9beff5aaf8b8086eaae0ce2034ed5ace0ab53223
illegi9 <illegi1503@gmail.com>
2023-02-13 18:16:04 +0300

    js 1.1

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ function showOpeningHours() {
     let getOpenOrClosed = ["Офис открыт", "Офис закрыт"];
 
     if (hours >= 9 && hours < 18) {
-        alert(getOpenOrClosed[0]);
+        return getOpenOrClosed[0];
     } else {
-        alert(getOpenOrClosed[1]);
+        return getOpenOrClosed[1];
     }
 }
 /* Функция которая исходя из текущего времени говорит пользователю:
@@ -21,14 +21,11 @@ function showDayOff() {
     let day = date.getDay();
     let daysOfTheWeek = ["Вс", 'Пн', "Вт", "Ср", "Чт", "Пт", "Сб"];
     let days = daysOfTheWeek[day];
-    let offOrWeek = ['Выходной день', 'Рабочий день']
 
-    if (days === daysOfTheWeek[6]) {
-        alert(offOrWeek[0]);
-    } else if (days === daysOfTheWeek[0]) {
-        alert(offOrWeek[0]);
-    } else {
-        alert(offOrWeek[1]);
+    if (days === daysOfTheWeek[6] || days === daysOfTheWeek[0]) {
+        alert('Выходной день');
+    }  else if (days != daysOfTheWeek[6] || days != daysOfTheWeek[0])  {
+        alert(showOpeningHours());
     }
 }
 /* Функция говорит пользователю о том что в субботу и воскресенье в офисе выходной */
